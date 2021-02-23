62ab59e1701c3a4132229eba24ac54e8733a16e3
Alexander Angulo <alex.anguloh@gmail.com>
2021-02-23 04:58:49 -0600

    Add Cors

--- src/index.js
+++ src/index.js
@@ -12,17 +12,23 @@ console.log(process.ENV)
 // settings
 app.set('port', process.env.PORT || 3000);
 
+var allowedOrigins = ['http://localhost:4200',
+                      'https://telynet.herokuapp.com'];
 // middlewares
 app.use(morgan('dev'));
-app.use(cors());
-// Configurar cabeceras y cors
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
